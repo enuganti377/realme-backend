9dc4c24f22b130b66e06bf222b93e659b9b9f597
ENUGANTI KARTHIK <enugantikarthikyadav@gmail.com>
2026-03-05 23:41:16 +0530

    Update cat.js

--- routes/cat.js
+++ routes/cat.js
@@ -4,17 +4,17 @@ const News = require("../models/News");
 
 router.get("/", async (req, res) => {
   try {
+
     const category = req.query.category;
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 5;
 
-    if (!category) {
-      return res.status(400).json({ message: "category is required" });
-    }
+    // If category exists → filter
+    const filter = category ? { category } : {};
 
-    const news = await News.find({ category })
+    const news = await News.find(filter)
       .sort({ publishedAt: -1 })
-      .skip((page - 1) * limit)  
+      .skip((page - 1) * limit)
       .limit(limit);
 
     res.json(news);
@@ -22,21 +22,6 @@ router.get("/", async (req, res) => {
   } catch (err) {
     console.log("Pagination error:", err);
     res.status(500).json({ message: "server error" });
-  }
-});
-
-router.get("/news/:id", async (req, res) => {
-  try {
-    const news = await News.findById(req.params.id);
-
-    if (!news) {
-      return res.status(404).json({ message: "News not found" });
-    }
-
-    res.json(news);
-  } catch (err) {
-    console.log("Single news error:", err);
-    res.status(500).json({ message: "Server error" });
   }
 });
 
